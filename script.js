0d1496ca6164da76fedccedf161dc6a981b02f31
samhirsch0109 <144719425+samhirsch0109@users.noreply.github.com>
2024-01-23 01:37:09 +0000

    fixed html + js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function year2001(){
     document.getElementById('response-text').outerText = '2001...';
-    document.getElementById('imageChange1').src = 'imgs/Unknown.jpeg'
-    document.getElementById('changeText1').innerText='hello can you pls work (change)'
+    document.getElementById('imageChange1').src = 'imgs/ITunes_12_logo.png';
 }
