0926a9a7c4b86e909103579a17bb8e1a1a2c6dc8
samhirsch0109 <144719425+samhirsch0109@users.noreply.github.com>
2024-01-23 03:01:21 +0000

    fixed js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function year2001(){
-    document.getElementById('response-text').outerText = '2001...';
+    document.getElementById('response-text').outerText = '2001...'
     document.getElementById('imageChange1').src = 'imgs/ITunes_12_logo.png';
 }
