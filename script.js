8738f52f3a547d03ee2718b75eef6b34d5228431
samhirsch0109 <144719425+samhirsch0109@users.noreply.github.com>
2024-01-24 02:48:13 +0000

    added hovers and js

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function year2001(){
-    document.getElementById('response-text').outerText = '2001...'
-    document.getElementById('imageChange1').src = 'imgs/ITunes_12_logo.png';
+function year1861(){
+    document.getElementById('response-text').innerText = '1861...'
+    document.getElementById('imageChange1').src = '';
+    document.getElementById('changeText').innerText = ''
+    document.getElementById('changeText2').innerText = ''
+    document.getElementById('imageChange2').src = ''
 }
+
+
+function year1913(){
+    document.getElementById('response-text').innerText = '1913...'
+    document.getElementById('imageChange1').src = '';
+    document.getElementById('changeText').innerText = ''
+    document.getElementById('changeText2').innerText = ''
+    document.getElementById('imageChange2').src = ''
+}
+
+function year1982(){
+    document.getElementById('response-text').innerText = '1982...'
+    document.getElementById('imageChange1').src = '';
+    document.getElementById('changeText').innerText = ''
+    document.getElementById('changeText2').innerText = ''
+    document.getElementById('imageChange2').src = ''
+}
+
+function year2001(){
+    document.getElementById('response-text').innerText = '2001...'
+    document.getElementById('imageChange1').src = 'imgs/ITunes_12_logo.png';
+    document.getElementById('changeText').innerText = ''
+    document.getElementById('changeText2').innerText = ''
+    document.getElementById('imageChange2').src = ''
+}
+
