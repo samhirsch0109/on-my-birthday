29f1c9cac92f304ccead6927cc5f252a30750a8c
Samantha Hirsch <sammyhirsch0109@gmail.com>
2024-01-26 14:58:18 -0500

    mostly done

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 function year1861(){
     document.getElementById('response-text').innerText = '1861...'
     document.getElementById('imageChange1').src = 'imgs/testing.jpg';
-    document.getElementById('changeText').innerText = ''
-    document.getElementById('changeText2').innerText = ''
+    document.getElementById('changeText').innerText = 'On January 9th 1861, Mississippi became the second US state to secede from the union'
+    document.getElementById('changeText2').innerText = 'They left the union because of Mississippis views of slavery.'
     document.getElementById('imageChange2').src = 'imgs/mississippi2.jpg'
 }
 
 
 function year1913(){
     document.getElementById('response-text').innerText = '1913...'
-    document.getElementById('imageChange1').src = '';
-    document.getElementById('changeText').innerText = ''
-    document.getElementById('changeText2').innerText = ''
-    document.getElementById('imageChange2').src = ''
+    document.getElementById('imageChange1').src = 'imgs/young_nixon.jpg';
+    document.getElementById('changeText').innerText = 'On January 9th 1913, Richard Nixon was born'
+    document.getElementById('changeText2').innerText = 'Nixon was the 37th president of the United States from 1969 to 1974'
+    document.getElementById('imageChange2').src = 'imgs/richard nixon.jpg'
 }
 
 function year1982(){
     document.getElementById('response-text').innerText = '1982...'
-    document.getElementById('imageChange1').src = '';
-    document.getElementById('changeText').innerText = ''
-    document.getElementById('changeText2').innerText = ''
-    document.getElementById('imageChange2').src = ''
+    document.getElementById('imageChange1').src = 'imgs/katemiddleton.jpg';
+    document.getElementById('changeText').innerText = 'On January 9th 1982, Catherine Middleton was born in Reading, Berkshire, England'
+    document.getElementById('changeText2').innerText = 'Kate and Prince William got married and she became the duchess of Cambridge'
+    document.getElementById('imageChange2').src = 'imgs/kateandwilliam.jpg'
 }
 
 function year2001(){
     document.getElementById('response-text').innerText = '2001...'
-    document.getElementById('imageChange1').src = 'imgs/ITunes_12_logo.png';
-    document.getElementById('changeText').innerText = ''
-    document.getElementById('changeText2').innerText = ''
-    document.getElementById('imageChange2').src = ''
+    document.getElementById('imageChange1').src = 'imgs/Unknown.jpeg';
+    document.getElementById('changeText').innerText = 'On January 9th 2001, Apple introduced iTunes. Steve jobs made the announcement'
+    document.getElementById('changeText2').innerText = 'iTunes helped with the debut of the iPod which later revolutionized digital music'
+    document.getElementById('imageChange2').src = 'imgs/stevejobs.jpg'
 }
 
